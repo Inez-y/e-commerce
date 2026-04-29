ec8fe40f67e6d06d1d4f23364b4b42d5a2c232d3
Inez Yoon <inez3cs@gmail.com>
2026-04-28 22:59:32 -0700

    Create order routes scratch

---
+++ src/orders/order.routes.ts
@@ -0,0 +1,14 @@
+/**
+ * POST /orders
+ * Customer creates an order
+ */
+
+/**
+ * GET /orders
+ * Admin sees all orders, customer sees own orders
+ */
+
+/**
+ * GET /orders/:id
+ * Admin can see any order, customer only their own order
+ */
