3b6891ef3f0ee238d101c7d5f4a19869fc0357ab
Inez Yoon <inez3cs@gmail.com>
2026-04-29 22:09:38 -0700

    Create a notification queue with redis

---
+++ src/jobs/notification.queue.ts
@@ -0,0 +1,32 @@
+import { Queue } from 'bullmq';
+import { redisConnection } from './redis';
+
+export type NotificationJobData = {
+    notificationId: string;
+};
+
+export const notificationQueue = new Queue<NotificationJobData>(
+    'notifications',
+    {
+        connection: redisConnection,
+    }
+);
+
+export async function enqueueNotification(notificationId: string) {
+    await notificationQueue.add(
+        'send-notification',
+        {
+            notificationId,
+        },
+        {
+            attempts: 3,
+            // wait longer between retries
+            backoff: {
+                type: 'exponential',
+                delay: 1000,
+            },
+            removeOnComplete: true,
+            removeOnFail: false,
+        }
+    );
+};
