d3f6b5fcafa6f63f683f0ab5324b2b011f6aa726
Inez Yoon <inez3cs@gmail.com>
2026-05-07 16:52:03 -0700

    Add playwright for e2e test

---
+++ storefront/e2e/playwright.config.ts
@@ -0,0 +1,32 @@
+// Backend still needs to be running separately
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  timeout: 30_000,
+  expect: {
+    timeout: 10_000,
+  },
+  fullyParallel: false,
+  retries: process.env.CI ? 2 : 0,
+  reporter: 'html',
+
+  use: {
+    baseURL: 'http://localhost:3001',
+    trace: 'on-first-retry',
+  },
+
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3001',
+    reuseExistingServer: !process.env.CI,
+    timeout: 60_000,
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
